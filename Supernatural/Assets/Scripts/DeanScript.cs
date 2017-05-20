8a91b612506210f47f43be3a2abff9369b42dadc
Aileen <aileen.w.zeng@gmail.com>
2017-05-19 17:14:30 -0700

    Trying to get jumping to work. Weird stuff is happening with RigidBody...

--- Supernatural/Assets/Scripts/DeanScript.cs
+++ Supernatural/Assets/Scripts/DeanScript.cs
@@ -14,7 +14,7 @@ public class DeanScript : MonoBehaviour {
 	private static readonly string SHOOT = "shoot";
 
 	//player mechanics
-	private Rigidbody rb;
+	private Rigidbody2D rb;
 	public float horizSpeed;
 	public float jumpSpeed;
 	public float gravity;
@@ -23,7 +23,7 @@ public class DeanScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -35,27 +35,38 @@ public class DeanScript : MonoBehaviour {
 
 	//Controls player movement
 	void movePlayer() {
+		//AUTOMATIC
+		//Tell if there is anything in a sphere shape below the player
+		RaycastHit hitInfo;
+		//isGrounded = Physics.SphereCast(rb.position, 0.51f, Vector3.down, out hitInfo, GetComponent<Collider2D>().bounds.size.y / 2, groundLayers);
+		Debug.Log (isGrounded);
+		isGrounded = Physics.Raycast(transform.position, Vector2.down, GetComponent<Collider2D>().bounds.size.y / 2);
 
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
+		//adds gravity to the player
+		rb.AddForce(Vector2.down * gravity * rb.mass);
+
+		//
 		//moves player right
 		if (Input.GetButton(RIGHT) == true) 
 		{
-			Debug.Log ("D pressed");
+			//Debug.Log ("D pressed");
 			transform.Translate(Vector2.right * horizSpeed  * Time.deltaTime);
 
 		}
 
+		//PLAYER INPUT
 		//moves player left
 		if (Input.GetButton (LEFT) == true) 
 		{
-			Debug.Log ("A pressed");
+			//Debug.Log ("A pressed");
 			transform.Translate (Vector2.left * horizSpeed * Time.deltaTime);
 		}
 
 		//makes player jump
-		if (Input.GetButton (JUMP) == true) 
+		if (Input.GetButton (JUMP) == true && isGrounded) 
 		{
-			Debug.Log ("W pressed");
-			//rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+			rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
 		}
 
 		//makes player shoot
@@ -64,9 +75,5 @@ public class DeanScript : MonoBehaviour {
 			Debug.Log ("SPACE pressed");
 			//create code to release a bullet - create class for it
 		}
-		//Tell if there is anything in a sphere shape below the player
-		//RaycastHit hitInfo;
-		//isGrounded = Physics.SphereCast(rb.position, 0.75f, Vector3.down, out hitInfo, GetComponent<Collider>().bounds.size.y / 2, groundLayers);
-		//rb.AddForce(Vector2.down * gravity * rb.mass);
 	}
 }
