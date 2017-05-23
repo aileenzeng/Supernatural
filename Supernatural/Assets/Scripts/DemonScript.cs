50584a7cd810aeb4b09cea8b1f19e155e902a1ac
Aileen <aileen.w.zeng@gmail.com>
2017-05-23 12:45:37 -0700

    working on demon speed hahah

--- Supernatural/Assets/Scripts/DemonScript.cs
+++ Supernatural/Assets/Scripts/DemonScript.cs
@@ -16,15 +16,16 @@ public class DemonScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		direction = true;
 		gameTicks = 0.0f;
 
 		rb = GetComponent<Rigidbody2D> ();
 		ts = GetComponent<Transform> (); 
 
 		rb.freezeRotation = true;
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		gameTicks += Time.deltaTime;
@@ -35,6 +36,27 @@ public class DemonScript : MonoBehaviour {
 
 	//moves demon left and right
 	void moveDemon() {
+		//if the demon is moving right and it has not gone to the right boundary
+		if (direction && (this.ts.transform.position.x < rightBoundary)) 
+		{
+			this.ts.Translate (Vector2.right * DemonSpeed * Time.deltaTime);
+			Debug.Log ("Enemy is moving right");
+			if (this.transform.position.x == rightBoundary) {
+				direction = !direction;
+				Debug.Log ("changing direction");
+			}
+		} 
+		//if the demon is moving left and it has not gone to the left boundary
+		else if (!direction && (this.ts.transform.position.x > leftBoundary)) 
+		{
+			this.ts.Translate (Vector2.left * DemonSpeed * Time.deltaTime);
+			direction = !direction;
+			if (this.transform.position.x == leftBoundary) {
+				direction = !direction;
+				Debug.Log ("Changing direction");
+			}
+			Debug.Log ("Demon is moving left");
+		}
 
 	}
 
